c9237a2ad9a96af85903b6d4810041073c653323
onikarubi <printuserselect@gmail.com>
2025-01-08 14:20:46 +0900

    init commit.

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,8 @@
+import SignUpForm from "@/features/auth/components/signup-form";
+
+export default function SignUpPage() {
+  return (
+    <SignUpForm />
+  )
+}
+
---
+++ src/components/layouts/auth-layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="container mx-auto px-4 py-8 flex justify-center items-center min-h-screen">
+      {}
+    </div>
+  )
+}
+
+export default AuthLayout;
