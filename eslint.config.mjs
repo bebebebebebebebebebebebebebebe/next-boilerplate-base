9dc995a24ab0b59440b467e58dffe7f040ad027a
onikarubi <printuserselect@gmail.com>
2025-03-23 13:48:12 +0900

    feat: 認証機能を追加
    - Google認証を実装
    - APIクライアントに認証機能を追加
    - 環境変数でGoogle認証のクライアントIDとシークレットを設定
    - セッション管理にJWTを使用

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,7 +26,7 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "off"
     },
     ignores: [
-      "node_modules/*",
+      "**/node_modules/**",
     ],
     languageOptions: {
       parser: "@typescript-eslint/parser",
